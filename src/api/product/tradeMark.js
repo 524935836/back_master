b02261f1d3f87e73f4ef84abd2757d1bc92f32db
zxk <524935836@qq.com>
2022-01-26 21:46:11 +0800

    添加品牌

--- src/api/product/tradeMark.js
+++ src/api/product/tradeMark.js
@@ -1,3 +1,11 @@
 import request from '@/utils/request'
 
 export const reqTradeMarkList = (page, limit) => request(`/admin/product/baseTrademark/${page}/${limit}`)
+
+export const reqAddOrUpdateTradeMark = (tradeMark) => {
+  if (tradeMark.id) {
+    return request({ url: '/admin/product/baseTrademark/update', data: tradeMark, method: 'put' })
+  } else {
+    return request({ url: '/admin/product/baseTrademark/save', data: tradeMark, method: 'post' })
+  }
+}
--- src/views/product/Trademark/index.vue
+++ src/views/product/Trademark/index.vue
@@ -4,7 +4,7 @@
       type="primary"
       icon="el-icon-plus"
       style="margin: 10px 0px"
-      @click="addTradeMark"
+      @click="showAddTradeMark"
     >添加</el-button>
     <!-- table表格 -->
     <el-table style="width: 100%" border :data="list">
@@ -24,7 +24,7 @@
           type="warning"
           icon="el-icon-edit"
           size="mini"
-          @click="updateTradeMark"
+          @click="showUpdateTradeMark"
         >修改</el-button>
         <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
       </el-table-column>
@@ -45,16 +45,18 @@
     <el-dialog title="添加品牌" :visible.sync="dialogFormVisible">
       <el-form style="width:80%">
         <el-form-item label="品牌名称" label-width="100px">
-          <el-input autocomplete="off"></el-input>
+          <el-input v-model="tmForm.tmName" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="品牌LOGO" label-width="100px">
           <!-- Upload 上传 -->
           <el-upload
             class="avatar-uploader"
-            action="https://jsonplaceholder.typicode.com/posts/"
+            action="/dev-api/admin/product/fileUpload"
             :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
           >
-            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <img v-if="tmForm.logoUrl" :src="tmForm.logoUrl" class="avatar" />
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
           </el-upload>
@@ -62,7 +64,7 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addOrUpdateTradeMark">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -77,7 +79,11 @@ export default {
       pageSize: 3,
       totalCount: 0,
       list: [],
-      dialogFormVisible: false
+      dialogFormVisible: false,
+      tmForm: {
+        tmName: '',
+        logoUrl: ''
+      }
     }
   },
   mounted() {
@@ -104,12 +110,39 @@ export default {
       return (this.pageNum - 1) * this.pageSize + 1 + index
     },
     // 添加品牌回调
-    addTradeMark() {
+    showAddTradeMark() {
       this.dialogFormVisible = true
+      this.tmForm = { tmName: '', logoUrl: '' }
     },
     // 修改品牌
-    updateTradeMark() {
+    showUpdateTradeMark() {
       this.dialogFormVisible = true
+    },
+    // 上传图片成功之后的回调
+    handleAvatarSuccess(res, file) {
+      this.tmForm.logoUrl = res.data
+    },
+    // 上传之前的1回调
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    },
+    // 添加或更新品牌
+    async addOrUpdateTradeMark() {
+      this.dialogFormVisible = false
+      const res = await this.$API.tradeMark.reqAddOrUpdateTradeMark(this.tmForm)
+      if (res.code === 200) {
+        this.$message.success(this.tmForm.id ? '修改品牌成功' : '添加商品成功')
+        this.getTradeMarkList()
+      }
     }
   }
 }
